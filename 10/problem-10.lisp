e03e92838dcd689d403e68be7bf49168cd5f3fe4
David Morris <davidrowlandmorris@gmail.com>
2012-11-17 04:38:04 +0000

    Fast solution to problem 10

--- 10/problem-10.lisp
+++ 10/problem-10.lisp
@@ -2,30 +2,36 @@
 
 ;; Finding the sum of the primes up to 2 million
 
-(defun multiple-of (n x)
-  "Return true if X is a multiple of N, NIL otherwise."
-  (eq 0 (mod x n)))
+(defun eliminate (sieve number)
+  "Given a SIEVE (binary vector) and a NUMBER, set bits corresponding
+to integer multiples of that number to 0."
+  (loop 
+     for i
+     from (* number 2)                  ;second multiple
+     to (1- (length sieve))             ;0-based indexing
+     by number
+     do (setf (sbit sieve i) 0)))
 
-(defun multiple-of-none (ns x)
-  "Return true if X is a multiple of none of the elements of NS, NIL
-otherwise."
-  (loop for f in ns
-     never (multiple-of f x)))
+(defun do-sieving (sieve)
+  "Run the Sieve of Erastothenes across a given bit-vector."
+  (loop 
+     for i
+     from 2                             ;first prime
+     to (sqrt (length sieve))           ;we can stop at sqrt(n)
+     when (= 1 (sbit sieve i))
+     do (eliminate sieve i)))
 
-(defun next-prime (lastprime primes)
-  "Given a list of PRIMES and the LASTPRIME (greatest prime), produce
-the next prime."
-  (do ((x (+ lastprime 2)
-          (+ x 2)))
-      ((multiple-of-none primes x) x)))
-
-(defun primes-upto (n)
-  "Generate a list of the primes up to N."
-  (do ((lastprime 5 (next-prime lastprime primes))
-       (primes '(2 3)
-               (append primes (list lastprime))))
-      ((> lastprime n) primes)))
+(defun find-primes (n)
+  "Return a list of all the prime numbers less than N."
+  (let ((sieve (make-array n
+                       :element-type 'bit
+                       :initial-element 1)))
+    (do-sieving sieve)
+    ;; collect numbers corresponding to set bits
+    (loop for i from 2 to (1- n)
+       when (= 1 (sbit sieve i))
+       collect i)))
 
 (defun problem-10 (n)
-  (apply #'+ (primes-upto n)))
-  
+  "Sum all the prime numbers less than N."
+  (apply #'+ (find-primes n)))
