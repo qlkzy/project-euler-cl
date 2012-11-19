983311d04cd45f4e63af469a661bfe00dc29892c
David Morris <davidrowlandmorris@gmail.com>
2012-11-19 08:48:43 +0000

    Sensible version of prob 1

--- problem-001.lisp
+++ problem-001.lisp
@@ -24,3 +24,17 @@ which are multiples of 3 or 5."
          (loop for x from 1 to (- n 1)
             collect (x-if-multiple-of-3-or-5 x))))
   
+
+;;; alternative implementation
+
+(defun factorp (f x)
+  "Return true if F is a factor of X."
+  (= 0 (mod x f)))
+  
+(defun problem-1-concise (n)
+  "Return the sum of the natural numbers below N
+which are multiples of 3 or 5."
+  (loop for x from 1 below n
+     when (or (factorp 3 x)
+              (factorp 5 x))
+     summing x))
