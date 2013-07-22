b3b19523f311a2ae914541d3b843f2cefcc74287
David Morris <davidrowlandmorris@gmail.com>
2013-07-22 21:18:37 +0100

    Solved problem 21

--- problem-021.lisp
+++ problem-021.lisp
@@ -7,29 +7,57 @@
   (and (= 0 (mod x d))
        (/= d x)))
 
-(defun sum-divisors (x)
-  "Return the number of divisors of X."
+(defun d (x)
+  "Return the sum of the divisors of X."
   (reduce #'+
           (loop for i from 1 to (1- x)
              when (divisor? i x)
              collect i)))
 
-(defun collate-divisors (n)
-  "Produce a list of cons cells containing in the car a number from 1
-to n, and in the cdr the sum of the divisors of that number."
-  (loop for i from 1 to n
-       collect (cons i (sum-divisors i))))
+(defun collate-ds (n)
+  "Produce an alist of pairs (x (d x)) for all x < N"
+  (loop for i from 1 to (1- n)
+       collecting (list i (d i))))
 
-(defun xref-divisors (n)
-  (let ((forward-map (make-array (list (* 2 n))
-                                 :initial-element 0))
-        (reverse-map (make-array (list (* 2 n))
-                                 :initial-element 0)))
-    (loop for x in (collate-divisors n) do
-         (progn (setf (aref forward-map (car x)) (cdr x))
-                (setf (aref reverse-map (cdr x)) (car x))))
-    (loop for i from 0 below (length forward-map)
-         when (= i (aref reverse-map (aref forward-map i)))
-         collect (cons i (aref forward-map i)))))
-                 
-         
+(defun order-within (al)
+  "Given an alist AL of lists of numbers, sort each element of AL
+such that its members are in order"
+  (loop for p in al
+       collecting (sort p #'<)))
+
+(defun sequence-lessp (seq1 seq2)
+  "Return T if seq1 is less than seq2"
+  (loop
+     for i in seq1
+     for j in seq2
+     when (< i j) return t
+     when (< j i) return nil
+     finally (return (<= i j))))
+
+(defun order-without (al)
+  "Given a list AL of lists of numbers, sort it according
+to the lexicographic ordering of the lists."
+  (sort al #'sequence-lessp))
+
+(defun duplicates-recur (l)
+  (when l
+    (if (equal (car l)
+               (cadr l))
+        (cons (car l) (duplicates (cdr l)))
+        (duplicates (cdr l)))))
+
+(defun duplicates (l)
+  "Given a sorted list L, return a list containing
+one instance of each duplicated element"
+  (remove-duplicates (duplicates-recur l) :test #'equal))
+
+(defun inner-sum (l)
+  (loop for e in l
+       summing (reduce #'+ e)))
+
+(defun problem-21 (n)
+  (inner-sum
+   (duplicates
+    (order-without
+     (order-within
+      (collate-ds n))))))
