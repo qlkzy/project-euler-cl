314d048108d20fa4d29f62f15a1b3e33527036a6
David Morris <davidrowlandmorris@gmail.com>
2014-02-24 19:03:38 +0000

    Problem 37

--- problem-037.lisp
+++ problem-037.lisp
@@ -1,10 +1,7 @@
 
 
 (defclass prime-generator ()
-  ((primes :initform '(2))))
-
-(defparameter foo
-  (make-instance 'prime-generator))
+  ((primes :initform '(2 3 5 7))))
 
 (defgeneric next-prime (gen))
 (defgeneric prime-p (gen p))
@@ -20,3 +17,37 @@
 
 (defmethod prime-p ((gen prime-generator) p)
   (member p (slot-value gen 'primes)))
+
+(defun left-truncate (x)
+  (mod x (expt 10 (floor (log x 10)))))
+
+(defun right-truncate (x)
+  (floor x 10))
+
+(defun left-truncations (x)
+  (loop for n = x then (left-truncate n)
+     until (= n 0)
+     collect n))
+
+(defun right-truncations (x)
+  (loop for n = x then (right-truncate n)
+     until (= n 0)
+     collect n))
+
+(defun truncations (x)
+  (append (left-truncations x)
+          (right-truncations x)))
+
+(defun all-truncations-prime (gen x)
+  (loop for y in (truncations x)
+       always (prime-p gen y)))
+
+(defun problem-37 ()
+  (reduce
+   #'+
+   (let ((gen (make-instance 'prime-generator)))
+     (loop for p = (next-prime gen)
+        with count = 0
+        until (>= count 11)
+        when (all-truncations-prime gen p)
+        collect p and do (incf count)))))
